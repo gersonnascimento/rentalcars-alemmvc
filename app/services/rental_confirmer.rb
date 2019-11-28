a98c06f0e3d52cdf39759da6fdfbe8cbfb11a018
Gerson Nascimento <gersonsilvadonascimento@hotmail.com>
2019-11-28 19:07:29 -0300

    Last day some more refactoring

--- app/services/rental_confirmer.rb
+++ app/services/rental_confirmer.rb
@@ -10,23 +10,29 @@ class RentalConfirmer
   def confirm
     return false if car.nil?
 
-    total_daily_hate = (car.category.daily_rate +
-                        car.category.third_party_insurance +
-                        car.category.car_insurance)
-
-    create_rental_item(car,total_daily_hate)
-
-    if addons = Addon.where(id: addon_ids)
-      addon_items = addons.map { |addon| addon.first_available_item }
-      addon_items.each do |addon_item|
-        rental.rental_items.create(rentable: addon_item, daily_rate: addon_item.addon.daily_rate)
-      end
-    end
-    rental.update(price_projection: rental.calculate_final_price)
+    create_rental_item(car, car.daily_rate)
+    create_rental_addon_items
+    update_price
     rental
   end
 
+  private
+
   def create_rental_item(item, daily_rate)
     rental.rental_items.create(rentable: item, daily_rate: daily_rate)
+  end
+
+  def create_rental_addon_items
+    addons = Addon.where(id: addon_ids)
+    return unless addons
+
+    addon_items = addons.map { |addon| addon.first_available_item }
+    addon_items.each do |addon_item|
+      rental.rental_items.create(rentable: addon_item, daily_rate: addon_item.addon.daily_rate)
+    end
+  end
+
+  def update_price
+    rental.update(price_projection: rental.calculate_final_price)
   end
 end
